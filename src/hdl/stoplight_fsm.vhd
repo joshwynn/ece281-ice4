d20167b74708c9e8f52bc9825e2587dd32adcb48
joshwynn <142031853+joshwynn@users.noreply.github.com>
2024-03-17 19:04:33 -0600

    Update stoplight_fsm.vhd
    completed spotlight_fsm.vhd ice 4

--- src/hdl/stoplight_fsm.vhd
+++ src/hdl/stoplight_fsm.vhd
@@ -60,36 +60,41 @@ library ieee;
   use ieee.numeric_std.all;
 
   
-entity stoplight_fsm is
+entity spotlight_fsm is
     Port ( i_C     : in  STD_LOGIC;
            i_reset : in  STD_LOGIC;
            i_clk   : in  STD_LOGIC;
            o_R     : out  STD_LOGIC;
            o_Y     : out  STD_LOGIC;
            o_G     : out  STD_LOGIC);
-end stoplight_fsm;
+end spotlight_fsm;
 
-architecture stoplight_fsm_arch of stoplight_fsm is 
+architecture stoplight_fsm_arch of spotlight_fsm is 
 	
 	-- create register signals with default state yellow (10)
+	signal f_Q, f_Q_next : std_logic_vector(1 downto 0) := "10";
   
 begin
 	-- CONCURRENT STATEMENTS ----------------------------
 	-- Next state logic
-	
-	
+	f_Q_next(0) <= ((not f_Q(1)) and (not f_Q(0)) and i_C) or ((not f_Q(1)) and f_Q(0) and i_C);
+	f_Q_next(1) <= (not f_Q(1)) and f_Q(0) and (not i_C);
 	-- Output logic
-	
+	o_G <= (not f_Q(1)) and f_Q(0);
+	o_R <= ((not f_Q(1)) and (not f_Q(0))) or (f_Q(1) and f_Q(0));
+	o_Y <= f_Q(1) and (not f_Q(0));
 	-------------------------------------------------------	
 	
 	-- PROCESSES ----------------------------------------	
 	-- state memory w/ asynchronous reset ---------------
-	register_proc : process (  )
-	begin
-			--Reset state is yellow
-
-
-	end process register_proc;
+	register_proc : process (i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            f_Q <= "10";        -- reset state is yellow
+        elsif (rising_edge(i_clk)) then
+            f_Q <= f_Q_next;    -- next state becomes current state
+        end if;
+    end process register_proc;
 	-------------------------------------------------------
 	
 end stoplight_fsm_arch;
